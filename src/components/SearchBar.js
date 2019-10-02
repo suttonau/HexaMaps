0050d353fc176805344121fff43d2a739f7540ed
Sean Sutton <sean@thinknimble.com>
2019-10-02 16:49:29 -0400

    adding state to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,17 @@ import React from 'react'
 import { View, TextInput, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.background}>
       <Feather name="search" size={30} style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)} />
     </View>
   )
 }
