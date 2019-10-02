74e5b9a1601f74e694c2be1ccab7b93062ec4e5c
Sean Sutton <sean@thinknimble.com>
2019-10-02 17:21:27 -0400

    handling errors

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,16 +7,22 @@ import yelp from '../services/api/yelp'
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [businesses, setBusinesses] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
 
   const _searchApi = async () => {
-    const response = await yelp.get('./search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'silver spring'
-      }
-    })
-    setBusinesses(response.data.businesses)
+    try {
+      const response = await yelp.get('./search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'silver spring'
+        }
+      })
+      setBusinesses(response.data.businesses)
+    } catch (err) {
+      console.log(err)
+      setErrorMessage('Something went wrong!')
+    }
   }
 
   return (
@@ -26,6 +32,7 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={_searchApi}
       />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {businesses.length} results</Text>
     </View>
   )
