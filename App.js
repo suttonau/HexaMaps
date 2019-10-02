cca41e7524681b1c506aae909baaf3d5fd2b4e37
Sean Sutton <sean@thinknimble.com>
2019-10-02 16:10:08 -0400

    eslint config

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import SearchScreen from './src/screens/SearchScreen'
 
-const navigator = createStackNavigator({ 
-  
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'HexaMap'
+  }
 })
+
+export default createAppContainer(navigator)
